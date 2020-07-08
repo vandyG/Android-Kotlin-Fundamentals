916ae7ab7f39ec52fae7b5f48eb2a5b2fc435df6
vandyG <vandy.goel23@gmail.com>
2020-07-08 17:56:22 +0530

    Add 1.2.5 display string
    Android Kotlin Fundamentals codelabs

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+    }
+
+    private fun rollDice(){
+//        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val resultText = findViewById<TextView>(R.id.result_text)
+        resultText.text = "Dice Rolled"
+    }
+}
---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.helloworld
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.helloworld.MESSAGE"
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        // Do something in response to button
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+}
