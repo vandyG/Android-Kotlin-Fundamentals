3fdd1ce9351a415885f6b8f74dd6b74545ec48d5
vandyG <vandy.goel23@gmail.com>
2020-07-14 18:12:30 +0530

    Dice Roller: Optimize dice image refrence

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,9 +10,14 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_Image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
@@ -24,9 +29,8 @@ class MainActivity : AppCompatActivity() {
 //        val resultText = findViewById<TextView>(R.id.result_text)
 //        resultText.text = random.toString()
 
-        val random = (1..6).random()
-        val diceImage = findViewById<ImageView>(R.id.dice_Image)
-        val drawableResource = when (random) {
+        //        val diceImage = findViewById<ImageView>(R.id.dice_Image)
+        val drawableResource = when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
