046ecf1dca2ca91f915a7fb4e69df072731ccca1
vandyG <vandy.goel23@gmail.com>
2020-07-18 17:38:45 +0530

    02.2: Add a click listner method to done button

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -14,6 +14,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickname(view: View){
         val editText = findViewById<EditText>(R.id.nickname_edit)
-//        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+
+        nicknameTextView.visibility = View.VISIBLE
     }
 }
