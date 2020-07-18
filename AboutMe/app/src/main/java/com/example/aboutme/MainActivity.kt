6fc2176f033aecd1a1aefd8cf79fd083b9442d37
vandyG <vandy.goel23@gmail.com>
2020-07-18 17:42:03 +0530

    02.2: Attach the click listener to the DONE Button

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.aboutme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
@@ -10,6 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
     }
 
     private fun addNickname(view: View){
