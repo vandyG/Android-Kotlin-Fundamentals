513b76b914cfe33a63c7204233981194d3f68a9e
vandyG <vandy.goel23@gmail.com>
2020-08-07 17:26:47 +0530

    05.4: Add transformation for the LiveData

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
@@ -19,11 +21,15 @@ class GameViewModel : ViewModel() {
         get() = _score
 
     //Countdown Time
-    private val _currentTime = MutableLiveData<Long>()
-    val currentTime: LiveData<Long>
-        get() = _currentTime
+    private val currentTime = MutableLiveData<Long>()
+//    val currentTime: LiveData<Long>
+//        get() = _currentTime
 
-    private val timer : CountDownTimer
+    val currentTimeString: LiveData<String> = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
+
+    private val timer: CountDownTimer
 
     // The list of words - the front of the list is the next word to guess
     lateinit var wordList: MutableList<String>
@@ -48,23 +54,22 @@ class GameViewModel : ViewModel() {
     init {
         _word.value = ""
         _score.value = 0
+        resetList()
+        nextWord()
 
-        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
             override fun onFinish() {
-                _currentTime.value = DONE
+                currentTime.value = DONE
                 onGameFinish()
             }
 
             override fun onTick(millisUntilFinished: Long) {
-                _currentTime.value = millisUntilFinished/ ONE_SECOND
+                currentTime.value = millisUntilFinished / ONE_SECOND
             }
-
         }
 
         timer.start()
 
-        resetList()
-        nextWord()
         Log.i("GameViewModel", "GameViewModel created!")
     }
 
