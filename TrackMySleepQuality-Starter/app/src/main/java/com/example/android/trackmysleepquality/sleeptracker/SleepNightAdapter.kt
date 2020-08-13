8466643986df80c496fc2be29623665cf0dc7057
vandyG <vandy.goel23@gmail.com>
2020-08-13 16:14:02 +0530

    07.1: Create ViewHolder for the new item

--- TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -2,7 +2,9 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
@@ -39,5 +41,10 @@ class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
         holder.textView.text = item.sleepQuality.toString()
 
     }
+}
 
+class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
+    val quality: TextView = itemView.findViewById(R.id.quality_string)
+    val qualityImage: ImageView = itemView.findViewById(R.id.quality_Image)
 }
