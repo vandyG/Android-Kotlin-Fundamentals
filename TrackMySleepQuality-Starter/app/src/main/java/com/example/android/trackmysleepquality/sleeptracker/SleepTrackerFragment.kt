bf939c6cb32447827725d1827051c7ee1295f0e1
vandyG <vandy.goel23@gmail.com>
2020-08-14 20:21:15 +0530

    07.3: Change the LayoutManager for the recycle view

--- TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -20,11 +20,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -63,6 +65,8 @@ class SleepTrackerFragment : Fragment() {
         binding.sleepList.adapter = adapter
         binding.lifecycleOwner = this
         binding.sleepTrackerViewModel = sleepTrackerViewModel
+
+        binding.sleepList.layoutManager = GridLayoutManager(activity, 3)
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer { nights ->
             adapter.submitList(nights)
